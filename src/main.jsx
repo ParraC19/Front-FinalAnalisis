517b4078d0b688143c8d2d3b4fdea4f932a53c58
aprendizeco <aprendizeco@gco.com.co>
2026-02-28 16:39:45 -0500

    fix: correcion encarpetado

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import FormLogInUsers from "./components/FormLogInUsers";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <FormLogInUsers />
   </StrictMode>,
 )
